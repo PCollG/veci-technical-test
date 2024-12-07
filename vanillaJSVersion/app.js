39dda393c8c1d743b59a6d5af0dae13f97e2f664
PCollG <paucollgarrido@gmail.com>
2024-12-07 14:00:46 +0100

    Add Item functionality

--- vanillaJSVersion/app.js
+++ vanillaJSVersion/app.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const addButton = document.querySelector("#js-add");
+  const listContainer = document.querySelector(".list");
   const modal = document.querySelector("#js-modal");
+
+  const addButton = document.querySelector("#js-add");
   const modalInput = modal.querySelector("input");
+  const modalAddButton = modal.querySelector("#js-modal-add");
   const modalCancelButton = modal.querySelector("#js-modal-cancel");
 
   // Toggle modal visibility
@@ -12,4 +15,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   modalCancelButton.addEventListener("click", toggleModal);
   addButton.addEventListener("click", toggleModal);
+
+  // Add item to the list
+  const addItem = (text) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("listItem");
+    listItem.textContent = text;
+
+    listContainer.appendChild(listItem);
+  };
+
+  modalAddButton.addEventListener("click", () => {
+    const text = modalInput.value.trim();
+    if (!text) {
+      alert("Cannot add an empty item.");
+      return;
+    }
+
+    addItem(text);
+    toggleModal();
+  });
 });
